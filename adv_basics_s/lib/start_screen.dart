36e461c61f18a1cd867f4234facf7e13df6ed07d
stayros130 <stayros130@gmail.com>
2023-10-18 18:25:57 +0300

    Made start screen more interesting

--- adv_basics_s/lib/start_screen.dart
+++ adv_basics_s/lib/start_screen.dart
@@ -16,38 +16,37 @@ class StartScreen extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Align(
-          alignment: const Alignment(0, -0),
         child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png', 
-                  width: 300,
-                  height: 300,
-                ),
-                const SizedBox(height: 80),
-                const Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 26,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Start Quiz',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255)
-                    ),
-                  ),
-                )
-              ]
-        ),
+          mainAxisSize: MainAxisSize.min, 
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(150, 255, 255, 255),
+            ),
+            // Opacity(opacity: 0.6,
+            //   child: Image.asset(
+            //     'assets/images/quiz-logo.png',
+            //     width: 300,
+            //   ),
+            // ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontSize: 26,
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ]),
       ),
-    ),
-   );
+    );
   }
 }
