aaa259bc323dc0f3b144c507bbd1e668a98ccd2c
stayros130 <stayros130@gmail.com>
2023-10-31 18:17:21 +0200

    It show the results but very ugly and a bit broken

--- adv_basics_s/lib/results_screen.dart
+++ adv_basics_s/lib/results_screen.dart
@@ -1,5 +1,8 @@
 
 
+import 'package:adv_basics_s/data/questions.dart';
+import 'package:adv_basics_s/questions_summery.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,6 +10,20 @@ class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'answer': chosenAnswers[i]});
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(context){
@@ -19,7 +36,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('you dummy. You believed to have answerd anyting correct? LOL, funny!'),
             const SizedBox(height: 30,),
-            const Text('Your wrong answers'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30,),
             TextButton(
               onPressed: () {}, 
