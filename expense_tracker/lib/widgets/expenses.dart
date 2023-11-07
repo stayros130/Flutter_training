85aacc6b3eb9d6d9e0c56425f2e9fc3d33beff38
stayros130 <stayros130@gmail.com>
2023-11-07 21:05:34 +0200

    Made the list nicer and appbar added but not fuctional yet

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -30,6 +30,15 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.add)
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text('The smth'),
