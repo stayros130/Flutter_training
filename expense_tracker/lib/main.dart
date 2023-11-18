77551876afa6198b295cb8c249d0e52ccbfa6454
stayros130 <stayros130@gmail.com>
2023-11-18 16:54:56 +0200

    Made it even prettier and getting ready for the chart

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -4,9 +4,28 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 32, 175, 156));
 
+var kDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: const Color.fromARGB(255, 5, 99, 125));
+
 void main() {
   runApp(
      MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal:16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style:ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
@@ -33,6 +52,7 @@ void main() {
         ),
         ),
       ),
+      //themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
