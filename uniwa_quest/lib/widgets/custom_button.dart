05df875f21f26230369ae5d6858c5b757babbe96
stayros130 <stayros130@gmail.com>
2023-12-07 21:41:57 +0200

    gonna try it as a windows application

--- uniwa_quest/lib/widgets/custom_button.dart
+++ uniwa_quest/lib/widgets/custom_button.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
+  const CustomButton({super.key,required this.buttonText, required this.onPressed});
+
   final String buttonText;
   final VoidCallback onPressed;
 
-  CustomButton({required this.buttonText, required this.onPressed});
-
   @override
   Widget build(BuildContext context) {
+    var buttonWidth = MediaQuery.of(context).size.width / 1.2;
+    var buttonHeight = MediaQuery.of(context).size.height / 12;
+  
+    //if ()
+
     return SizedBox(
       height: MediaQuery.of(context).size.height / 12, 
       width: MediaQuery.of(context).size.width / 1.2,
